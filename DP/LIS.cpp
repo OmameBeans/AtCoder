5d9aed1376c1f1ae880937c365a2db4f59c223a7
OmameBeans <hinodaichi@DESKTOP-O19OEIJ.localdomain>
2021-06-16 09:43:00 +0900

    new file:   DP/LIS.cpp 	new file:   DP/README.md 	modified:   DataStructure/SegmentTree.cpp

---
+++ DP/LIS.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for (int i = 0; i < (n); ++i)
+#define DREP(i,s,n) for(int i = (s); i < (n); i++)
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+using ll = long long;
+using P = pair<int,int>;
+using Pl = pair<long long,long long>;
+using veci = vector<int>;
+using vecl = vector<long long>;
+using vecveci = vector<vector<int>>;
+using vecvecl = vector<vector<long long>>;
+const int MOD = 1000000007;
+const double pi = acos(-1);
+ll gcd(ll a, ll b) {if(b == 0) return a; else return gcd(b,a%b);}
+ll lcm(ll a, ll b) {return a*b/gcd(a,b);}
+
+int LIS(vector<int> &A, int N,vector<int> &dp) {
+    const int INF = 1<<30;
+    dp = vector<int>(N+1,INF);
+    for(int i = 0; i < N; i++) {
+        auto itr = upper_bound(dp.begin(),dp.end(),A[i])-dp.begin();
+        dp[itr] = A[i];
+    }
+    for(int i = N-1; i >= 0; i--) {
+        if(dp[i] != INF) {
+            return i+1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int N; cin >> N;
+    veci A(N); REP(i,N) cin >> A[i];
+    veci dp;
+    cout << LIS(A,N,dp) << endl;
+    return 0;
+}
--- DataStructure/SegmentTree.cpp
+++ DataStructure/SegmentTree.cpp
@@ -14,6 +14,7 @@ struct SegTree {
         while(siz > x) x *= 2;
         n = x;
     }
+    //位置iの要素をxにする
     void set(int i, T x) {
         i += n-1;
         val[i] = x;
@@ -22,10 +23,17 @@ struct SegTree {
             val[i] = f(val[2*i+1],val[2*i+2]);
         }
     }
+    //位置iの要素をF(val[i],x)に置き換える
+    void update(int i, T x, Fx F) {
+        i += n-1;
+        val[i] = F(val[i],x);
+        while(i>0) i=(i-1)/2,val[i] = f(val[i*2+1],val[i*2+2]);
+    }
+    //[a,b)の要素にfを適用する
     T query(int a, int b) {return query_sub(a,b,0,0,n);}
     T query_sub(int a,int b,int k,int l,int r) {
         if(r <= a || b <= l) return e;
-        else if(a <= l && r <= b) val[k];
+        else if(a <= l && r <= b) return val[k];
         else {
             T v1 = query_sub(a,b,2*k+1,l,(l+r)/2);
             T v2 = query_sub(a,b,2*k+2,(l+r)/2,r);
