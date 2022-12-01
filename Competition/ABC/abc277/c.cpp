d281ca6774002fb5b76e7480942e40d8164487e5
OmameBeans <hinoddaichi1218@gmail.com>
2022-12-01 16:53:42 +0900

    update

---
+++ Competition/ABC/abc277/c.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for (int i = 0; i < (n); ++i)
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+using ll = long long;
+using P = pair<int,int>;
+using Pl = pair<long long,long long>;
+using veci = vector<int>;
+using vecl = vector<long long>;
+using vecveci = vector<vector<int>>;
+using vecvecl = vector<vector<long long>>;
+const int MOD = 1000000007;
+const double pi = acos(-1);
+ll gcd(ll a, ll b) {if(b == 0) return a; else return gcd(b,a%b);}
+ll lcm(ll a, ll b) {return a*b/gcd(a,b);}
+
+template<int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {if (val < 0) val += MOD;}
+    constexpr int getmod() { return MOD; }
+    constexpr Fp operator - () const noexcept {return val ? MOD - val : 0;}
+    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator += (const Fp& r) noexcept {val += r.val;if (val >= MOD) val -= MOD;return *this;}
+    constexpr Fp& operator -= (const Fp& r) noexcept {val -= r.val;if (val < 0) val += MOD;return *this;}
+    constexpr Fp& operator *= (const Fp& r) noexcept {val = val * r.val % MOD;return *this;}
+    constexpr Fp& operator /= (const Fp& r) noexcept {long long a = r.val, b = MOD, u = 1, v = 0;while (b) {long long t = a / b;a -= t * b; swap(a, b);u -= t * v; swap(u, v);}val = val * u % MOD;if (val < 0) val += MOD;return *this;}
+    constexpr bool operator == (const Fp& r) const noexcept {return this->val == r.val;}
+    constexpr bool operator != (const Fp& r) const noexcept {return this->val != r.val;}
+    friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {return os << x.val;}
+    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {if (n == 0) return 1;auto t = modpow(a, n / 2);t = t * t;if (n & 1) t = t * a;return t;}
+};
+
+using mint = Fp<MOD>;
+
+int main() {
+    int N; cin >> N;
+    veci A(N),B(N);
+    REP(i,N) {
+        cin >> A[i] >> B[i];
+    }
+    set<int> st;
+    map<int,int> cv, ori;
+    int cnt = 0;
+    REP(i,N) {
+        st.insert(A[i]);
+        st.insert(B[i]);
+    }
+    if(!st.count(1)) {
+        cout << 1 << endl;
+        return 0;
+    }
+    for(auto x : st) {
+        cv[x] = cnt;
+        ori[cnt] = x;
+        cnt++;
+    }
+
+    vecveci G(400010);
+    REP(i,N) {
+        G[cv[A[i]]].push_back(cv[B[i]]);
+        G[cv[B[i]]].push_back(cv[A[i]]);
+    }
+
+    veci seen(400010);
+
+    auto dfs = [&](auto dfs, int i) -> void {
+        seen[i] = 1;
+        for(auto ni : G[i]) {
+            if(seen[ni]) continue;
+            dfs(dfs,ni);
+        }
+    };
+
+    dfs(dfs,0);
+
+    int m = -1;
+
+    REP(i,400010) {
+        if(seen[i]) {
+            m = max(m,ori[i]);
+        }
+    }
+    cout << m << endl;
+    return 0;
+}
